ef1e83208e5759e09ce0da17b31aa7c54edca52f
Shumeng <60369045+HonestBook@users.noreply.github.com>
2022-11-18 05:47:35 +0000

    Connect to mysql server

--- london-tube.py
+++ london-tube.py
@@ -1,7 +1,28 @@
 import json
+import mysql.connector
   
+### load the json file
 f = open('train-network.json', 'r')
   
 data = json.load(f)
   
 print(data)
+
+### Establish a connection with the sql server
+
+username = input('Username: ')
+password = input('Password: ')
+
+cnx = mysql.connector.connect(user=username, password=password,
+                              host='127.0.0.1',
+                              database='london_tube')
+
+cnx.close()
+
+### Write data into the sql server
+
+### Continuously accept queries
+
+
+### Allow the user to quit
+
