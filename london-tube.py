79d36726cf9af95eba0d0de2aedad91668195d75
Shumeng <60369045+HonestBook@users.noreply.github.com>
2022-11-18 06:35:37 +0000

    Implement multiple credential attempts

--- london-tube.py
+++ london-tube.py
@@ -24,19 +24,29 @@ password = input('Password: ')
 
 
 # Create the connection with error handling
-try:
-  cnx = mysql.connector.connect(user=username,
-                                password=password,
-                                database=config['db_name'])
-  print('Successfully established connection with MySQL server')
-  print('Now using databse ' + config['db_name'])
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist, please check the configuration in config.yaml")
-  else:
-    print(err)
-else:
-  cnx.close()
+login_success = False
+
+# If wrong credentials were entered, ask again.
+while not login_success:
+    # Prompt the user to enter credentials
+    username = input('Username: ')
+    password = input('Password: ')
+    try:
+        cnx = mysql.connector.connect(user=username,
+                                        password=password,
+                                        database=config['db_name'])
+        print('Successfully established connection with MySQL server')
+        print('Now using databse ' + config['db_name'])
+        login_success = True 
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist, please check the configuration in config.yaml")
+            # if database name is wrong, there is no point in asking for credentials again
+            break
+        else:
+            print(err)
+    else:
+        cnx.close()
 
 ### Write data into the sql server
 
