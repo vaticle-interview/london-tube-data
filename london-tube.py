4f980446545f5802b4462d17d5b3422faabae7ad
Shumeng <60369045+HonestBook@users.noreply.github.com>
2022-11-18 06:16:31 +0000

    Handle wrong database credentials

--- london-tube.py
+++ london-tube.py
@@ -1,5 +1,6 @@
 import json
 import mysql.connector
+from mysql.connector import errorcode
   
 ### load the json file
 f = open('train-network.json', 'r')
@@ -10,14 +11,25 @@ print(data)
 
 ### Establish a connection with the sql server
 
+# Prompt the user to enter credentials
 username = input('Username: ')
 password = input('Password: ')
 
-cnx = mysql.connector.connect(user=username, password=password,
-                              host='127.0.0.1',
-                              database='london_tube')
 
-cnx.close()
+try:
+  cnx = mysql.connector.connect(user=username,
+                                password=password,
+                                database='london-tube')
+  print('Successfully established connection with MySQL server')
+except mysql.connector.Error as err:
+#   if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+#     print("Something is wrong with your user name or password")
+#   elif err.errno == errorcode.ER_BAD_DB_ERROR:
+#     print("Database does not exist")
+#   else:
+    print(err)
+else:
+  cnx.close()
 
 ### Write data into the sql server
 
