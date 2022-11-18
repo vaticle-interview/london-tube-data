588417583ab06b9672b170a9afc69f4397de62ac
Shumeng <60369045+HonestBook@users.noreply.github.com>
2022-11-18 21:49:34 +0000

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
+DROP TABLE IF EXISTS passes;
 DROP TABLE IF EXISTS stations;
 DROP TABLE IF EXISTS trainlines;
-DROP TABLE IF EXISTS passes;
 
 CREATE TABLE stations (
     `id` varchar(16) NOT NULL PRIMARY KEY,
@@ -12,7 +12,7 @@ CREATE TABLE trainlines (
     `name` varchar(64) NOT NULL
 ); 
 
-CREATE TABLE salaries (
+CREATE TABLE passes (
     `station_id` varchar(16) NOT NULL,
     `line_id` int(16) NOT NULL,
     PRIMARY KEY (`station_id`, `line_id`),
